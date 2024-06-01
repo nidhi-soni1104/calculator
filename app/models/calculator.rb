89c49e7a32b0c2bae1e0fee1f20e1e0df1a67ff8
Nidhi Soni <nsoni@redhat.com>
2024-06-01 16:26:18 +0530

    completed firt case test with method

--- app/models/calculator.rb
+++ app/models/calculator.rb
@@ -1,2 +1,5 @@
 class Calculator < ApplicationRecord
+	def self.add(numbers)
+		return 0 if numbers.empty?
+	end
 end
--- test/models/calculator_test.rb
+++ test/models/calculator_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class CalculatorTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "empty string returns 0" do
+    assert_equal 0, Calculator.add("")
+  end
+
+  test "single number returns the number" do
+    assert_equal 1, Calculator.add("1")
+  end
+
+  test "two numbers return their sum" do
+    assert_equal 6, Calculator.add("1,5")
+  end
 end
